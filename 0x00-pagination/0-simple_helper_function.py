01a37cb86cbd639a958b89f505e4e1bd0bcfb95a
Rachid KABBAB <r.kabbab@gmail.com>
2024-05-06 22:44:44 +0000

    Fixed the pycodestyle of the first task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 
 
 def index_range(page, page_size):
-    """ It returns a tuple of size two containing a start index and an end index
+    """ It returns a tuple of size containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
         particular pagination parameters. """
     if page and page_size:
